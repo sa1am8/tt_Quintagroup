df2b68ef1dffc73c5429e0dad518431fe639c931
Toshka <tinkerino571@gmail.com>
2022-12-28 21:27:52 +0200

    updated

--- additional.py
+++ additional.py
@@ -1,7 +1,8 @@
 import datetime
 
 API_LINK = "https://api.clockify.me/api/v1/"
-SECRET_KEY = "MGFhMjE1ZjItYzJkYS00OWE2LTk2NTctODgyNDhkNjVjNmI5"
+with open('secret') as file:
+    SECRET_KEY = file.readline()
 SECRET_HEADERS = {"X-Api-Key": SECRET_KEY}
 
 
@@ -24,12 +25,20 @@ def get_projects_link(workspace_id):
 def get_project_link(workspace_id, project_id):
     return API_LINK + f"/workspaces/{workspace_id}/projects/{project_id}"
 
+def get_users_link(workspace_id):
+    return API_LINK + f"/workspaces/{workspace_id}/users"
+
+def get_time_entries_link(workspace_id, user_id):
+    return API_LINK + f"/workspaces/{workspace_id}/user/{user_id}/time-entries"
+
+
 
 def normal_format_to_sec(duration):
+
     coef = {'S': 1, 'M': 60, 'H': 60 * 60}
     result = 0
     num = ''
-    print(duration)
+
     for i in duration[2:]:
 
         if i.isnumeric():
--- main.py
+++ main.py
@@ -25,19 +25,53 @@ def get_tasks_info(workspace_id: str, project_id: str) -> dict:
     r = requests.get(link, headers=SECRET_HEADERS)
     return r.json()
 
+
 def get_task_info(workspace_id: str, project_id: str, task_id) -> dict:
     link = get_task_link(workspace_id, project_id, task_id)
     r = requests.get(link, headers=SECRET_HEADERS)
     return r.json()
 
 
+def get_users(workspace_id):
+    link = get_users_link(workspace_id)
+    r = requests.get(link, headers=SECRET_HEADERS)
+    return r.json()
+
+
+def get_time_entries(workspace_id, user_id):
+    link = get_time_entries_link(workspace_id, user_id)
+    r = requests.get(link, headers=SECRET_HEADERS)
+    return r.json()
+
+
+
 
 if __name__ == "__main__":
     workspace_info = get_workspaces_info()
     total_time = 0
+    date = dict()
     for workspace in workspace_info:
         workspace_id = workspace.get('id')
         projects_info = get_projects_info(workspace_id)
+
+        users = get_users(workspace_id)
+        for user in users:
+            user_id = user.get('id')
+            time_entries = get_time_entries(workspace_id, user_id)
+
+            for time_entry in time_entries:
+
+                start = time_entry.get('timeInterval').get('start').split("T")[0]
+                duration = time_entry.get('timeInterval').get('duration')
+
+                if not duration:
+                    continue
+
+                if start not in date:
+                    date[start] = normal_format_to_sec(duration)
+                else:
+                    date[start] += normal_format_to_sec(duration)
+
 
         for project in projects_info:
             project_id = project.get('id')
@@ -54,3 +88,6 @@ if __name__ == "__main__":
 
     print()
     print('Total time duration: ', sec_to_normal_format(total_time))
+    print()
+    for i in date:
+        print(f'{i} - {sec_to_normal_format(date[i])}')
